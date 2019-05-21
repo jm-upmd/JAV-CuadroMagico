b2315894d36cdb8488d489c009aa5c990383cd0b
Jose M <jmdh@aytoalcobendas.org>
2019-05-21 16:38:28 +0200

    Nueva versión

--- src/cuadro_magico/CuadroMagico4k_v1.java
+++ src/cuadro_magico/CuadroMagico4k_v1.java
@@ -27,70 +27,84 @@ public class CuadroMagico4k_v1 {
 
 	// La dimensión de cuadro ha de ser N = 4K
 	static final int DIMEN_BASE = 4;
-	
+
 	static int[][] cuadroInicial; // array bidimensional con valores del cuadro inicial
 	static int[][] cuadroMagico; // array bidimensional con el cuadro mágico
-	
+
 	static Scanner teclado = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		
+
 		int dimen = 0;
-
+		
 		System.out.println("*** Generador de Cuadros Mágicos de dimensión NxN, con N=4K ***\n\n");
-		System.out.println("Introduce el valor de K: ");
-
-		// Pedimos por teclado el valor de K y validamos que es un entero mayor que 0.
-		if (teclado.hasNextInt() ) {
-			if ( (dimen = teclado.nextInt()) <= 0   ) {
-				terminar("K tiene que ser mayor o igual que 1");
+		
+		// Leer valor de k y valida que es un entero mayor que cero.
+		// Si no se le da un valor correcto lo vuelve a pedir 
+		
+		boolean terminar = false;
+		do {
+			System.out.println("Introduce el valor de K: ");
+			// Pedimos por teclado el valor de K y validamos que es un entero mayor que 0.
+			if (teclado.hasNextInt()) {
+				if ((dimen = teclado.nextInt()) <= 0) {
+					System.out.println("K tiene que ser mayor o igual que 1");
+					terminar = false;
+				} else 
+					terminar=true;
+			} else {
+				System.out.println("El dato introducido no es un número entero");
+				teclado.next();  // Leer el valor malo del buffer para sacarlo.
+				
 			}
-		} else {
-			terminar("El dato introducido no es un número entero");
-		}
+			
+		} while (!terminar);
 
 		dimen *= DIMEN_BASE; // dimensión del cuadro. cuadro es de dimen * dimen elementos.
 
 		// Genera cuadro inicial con valores consecutivos
 		generaCuadroInicial(dimen);
 
-		// Constantes que contienen la dimensión mayor y la menor de cada uno de los bloques en que se divide el cuadro
+		// Constantes que contienen la dimensión mayor y la menor de cada uno de los
+		// bloques en que se divide el cuadro
 
 		final int dimMenor = dimen / 4;
 		final int dimMayor = dimen / 2;
-		
-		// Columna donde comienzan bloques más a la derecha  (C,F,G) o fila
+
+		// Columna donde comienzan bloques más a la derecha (C,F,G) o fila
 		// donde comienzan bloques más abajo (G, H, I)
-		
-		final int posBloqDerAbaj = dimMayor + dimMenor + 1; 
+
+		final int posBloqDerAbaj = dimMayor + dimMenor + 1;
 
 		/*
-		 * Bloques A,C,G,I son de dimensiónes dimMenor filas x dimMenor columnas 
-		 * Bloques B,H son de dimensiones dimMenor filas x dimMayor columnas 
-		 * Bloques D,F son de dimensiones dimMayor filas x dimMenor columnas 
-		 * Bloque E es de dimensión dimMayor filas x dimMayor columnas
+		 * Bloques A,C,G,I son de dimensiónes dimMenor filas x dimMenor columnas Bloques
+		 * B,H son de dimensiones dimMenor filas x dimMayor columnas Bloques D,F son de
+		 * dimensiones dimMayor filas x dimMenor columnas Bloque E es de dimensión
+		 * dimMayor filas x dimMayor columnas
 		 */
 
 		// Creamos matriz para el cuadro mágico
 		cuadroMagico = new int[dimen][dimen];
 
-		
-		// Copiamos bloques de cuadro inical al cuadro mágico.		
-		// Copia bloques A, C, E, G, I en la misma posición en la matriz cuadromagico.		
-		//copiaBloque(columna, fila, numFilas, numColumnas)
+		// Copiamos bloques de cuadro inical al cuadro mágico.
+		// Copia bloques A, C, E, G, I en la misma posición en la matriz cuadromagico.
+		// copiaBloque(columna, fila, numFilas, numColumnas)
 
-		copiaBloque(1, 1, dimMenor, dimMenor);   // Bloque A. Copia bloque que comieza columna 1, fila 1, y tiene dimMenor filas y dimMenor columnas
-		copiaBloque(posBloqDerAbaj, 1, dimMenor, dimMenor); // Bloque C. 
-		copiaBloque(dimMenor + 1, dimMenor + 1, dimMayor, dimMayor);  //Bloque E
+		copiaBloque(1, 1, dimMenor, dimMenor); // Bloque A. Copia bloque que comieza columna 1, fila 1, y tiene dimMenor
+												// filas y dimMenor columnas
+		copiaBloque(posBloqDerAbaj, 1, dimMenor, dimMenor); // Bloque C.
+		copiaBloque(dimMenor + 1, dimMenor + 1, dimMayor, dimMayor); // Bloque E
 		copiaBloque(1, posBloqDerAbaj, dimMenor, dimMenor); // Bloque G
 		copiaBloque(posBloqDerAbaj, posBloqDerAbaj, dimMenor, dimMenor); // Bloque I
 
-		// Copia a cuadroMagico los bloques B,H,D,F simetricos respecto a cuadroInicial y volteados
+		// Copia a cuadroMagico los bloques B,H,D,F simetricos respecto a cuadroInicial
+		// y volteados
 		// horizontal y verticalmente.
-		//copiaBloqueSimetrico(colOrigen, filaOrigen, numFilas, numCol,	colDestino, filaDestino)
+		// copiaBloqueSimetrico(colOrigen, filaOrigen, numFilas, numCol, colDestino,
+		// filaDestino)
 
-		// Bloque B a posición de bloque H 
-		copiaBloqueSimetrico(dimMenor + 1, 1, dimMenor, dimMayor, dimMenor + 1, posBloqDerAbaj); 
+		// Bloque B a posición de bloque H
+		copiaBloqueSimetrico(dimMenor + 1, 1, dimMenor, dimMayor, dimMenor + 1, posBloqDerAbaj);
 		// Bloque H a posición de bloque B
 		copiaBloqueSimetrico(dimMenor + 1, posBloqDerAbaj, dimMenor, dimMayor, dimMenor + 1, 1);
 		// Bloque D a posición de bloque F
@@ -101,7 +115,7 @@ public class CuadroMagico4k_v1 {
 		// Muestra el cuadro mágico por consola
 		muestraCuadro(cuadroMagico);
 
-	}  // fin main()
+	} // fin main()
 
 	private static void generaCuadroInicial(int n) {
 		cuadroInicial = new int[n][n];
@@ -147,41 +161,40 @@ public class CuadroMagico4k_v1 {
 
 	// Copia los elementos en orden simétrico del bloque de cuadroOrigen a
 	// cuadroDestino
-	// 						a b 
-	//						e f 
-	// copia simetrica -->  
-	// 						f e
-	//                      b a
+	// a b
+	// e f
+	// copia simetrica -->
+	// f e
+	// b a
 
-	
-	private static void copiaBloqueSimetrico(int colOrigen, int filaOrigen, int numFilas, int numCol,
-			int colDestino, int filaDestino) {
-		
+	private static void copiaBloqueSimetrico(int colOrigen, int filaOrigen, int numFilas, int numCol, int colDestino,
+			int filaDestino) {
+
 		int yDest = filaDestino - 1;
 		int xDest = colDestino - 1;
 		int yOrg = filaOrigen + numFilas - 2;
 		int xOrg = colOrigen + numCol - 2;
-		
+
 		for (int i = yOrg; i >= filaOrigen - 1; i--) {
-			
+
 			for (int j = xOrg; j >= colOrigen - 1; j--) {
 				cuadroMagico[yDest][xDest] = cuadroInicial[i][j];
 				xDest++;
 			}
-			
+
 			yDest++;
 			xDest = colDestino - 1;
 
 		}
 
 	}
-	
+
 	private static void terminar(String mensaje) {
 		System.out.println(mensaje);
 		System.out.println("Programa finalizado");
 		teclado.close();
 		System.exit(0);
-		
+
 	}
 
 }
